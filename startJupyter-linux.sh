3f3df5369db71bf28ebfb2acc75aa6eac16de513
llathrop <llathrop@gmail.com>
2017-09-22 16:44:17 -0500

    feat:add some notes for easy jupyter start, test tensorflow works in notebook

---
+++ startJupyter-linux.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+source activate aind2
+jupyter notebook --ip=0.0.0.0 --no-browser
+
---
+++ startjupyter.bat
@@ -0,0 +1,5 @@
+@echo on
+c:
+cd C:\Users\llathrop\Documents\projects\udacity-AIND\class_projects\aind2-dog-project\
+set KERAS_BACKEND=tensorflow
+python "C:/ProgramData/Anaconda3/Scripts/jupyter-notebook-script.py" %USERPROFILE%\Documents\projects
