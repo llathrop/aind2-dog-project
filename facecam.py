eaf23f7591a8b8ac2346fba1a7e693b8398b4c50
llathrop <llathrop@gmail.com>
2017-09-29 11:32:54 -0500

    fix: add notes, spacings, etc

--- facecam.py
+++ facecam.py
@@ -1,6 +1,9 @@
 import cv2
 import sys
 
+image_path='test_images/'
+frame_saver=[]
+
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
@@ -9,6 +12,7 @@ video_capture = cv2.VideoCapture(0)
 while True:
 	# Capture frame-by-frame
 	ret, frame = video_capture.read()
+	orig=frame.copy()
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -32,7 +36,24 @@ while True:
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
+		
+	if cv2.waitKey(1) & 0xFF == ord('s'):
+		frame_saver.append(orig)
+		
 
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
+
+count=0
+for frame in frame_saver:
+	r = 1350.0 / frame.shape[1]
+	dim = (1350, int(frame.shape[0] * r))
+	frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+	cv2.imwrite(image_path+'test-'+str(count)+'.png',frame)
+	count+=1
+	cv2.imshow('image',frame)
+	cv2.waitKey(0)
+	
+cv2.destroyAllWindows()	
+	
